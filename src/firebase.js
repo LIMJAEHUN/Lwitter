8680ab4af60fe1ad5238633aae5cc8ac124baf93
LIMJAEHUN <ijhgun007@naver.com>
2022-07-12 21:50:28 +0900

    '1.0ver'

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBnupPa3o4DG6ls9op5rOb-riIPpkYKKM4",
+    authDomain: "jwitter2.firebaseapp.com",
+    projectId: "jwitter2",
+    storageBucket: "jwitter2.appspot.com",
+    messagingSenderId: "183781658715",
+    appId: "1:183781658715:web:92a81ece996f1a9100ef7e"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
