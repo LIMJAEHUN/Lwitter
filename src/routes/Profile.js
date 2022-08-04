0040cfecbf5c677381cd0f4f6b653f0c788228f3
LIMJAEHUN <ijhgun007@naver.com>
2022-08-04 21:36:49 +0900

    100

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {  useState, useEffect } from 'react';
-import { authService,db } from 'fbase.js';
+import {  useState } from 'react';
+import { authService } from 'fbase.js';
 import { useHistory } from "react-router-dom"; //useHistory를 이용한 로그아웃
 import { updateProfile } from "@firebase/auth"
-import { collection, query, where,orderBy,getDocs  } from "firebase/firestore";
 
 
 const Profile = ({refreshUser , userObj }) => {
 
     const history = useHistory();
     const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
-    const [ lweets, setLweets ] = useState([]);
+  
 
     
     const onLogOutClick = () =>{ 
@@ -25,25 +24,7 @@ const Profile = ({refreshUser , userObj }) => {
         setNewDisplayName(value);
     };
 
-    const getMyLweets = async() => {
-       const lweets = query(collection(db,"lweets"),where("creatorID", "==", userObj.uid),orderBy("createdAt", "desc"));
-     //  console.log("Document data:", lweets.getDoc.data());
-       // console.log(doc.id, " => ", doc.data());
-      // console.log(lweets.docs.map((doc) => doc.data()));
-        
-      const querySnapshot = await getDocs(lweets);
-      querySnapshot.forEach((doc) => {
-        const MYLweetsObjects = { ...document.data(), id: document.id};
-        setLweets((prev) => [MYLweetsObjects, ...prev])
-
-      console.log(doc.id, "=>", doc.data());
-      });
-     
-    };
-
-    useEffect(() => {
-        getMyLweets();
-    }, []);
+zzzz
 
 
 
@@ -63,13 +44,6 @@ const Profile = ({refreshUser , userObj }) => {
             <input type = "submit" value="update Profile" />
         </form>
         <button onClick={onLogOutClick}>Log Out </button>
-        <div>
-            {lweets.map((lweet) => (
-                <div key={lweet.id}>
-                    <h4>{lweet.text}</h4>
-                </div>
-            ))}
-        </div>
         
         
         </>
