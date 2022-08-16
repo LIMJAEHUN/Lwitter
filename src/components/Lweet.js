7af59fb55a1894b49d250cc10c520c076e43bf92
LIMJAEHUN <ijhgun007@naver.com>
2022-08-16 23:01:15 +0900

    저장기능 미작동

--- src/components/Lweet.js
+++ src/components/Lweet.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import { db,storage } from "fbase.js";
-import { doc, deleteDoc,  updateDoc } from "firebase/firestore";
+import {addDoc, doc, deleteDoc,  updateDoc,onSnapshot,collection } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage"
 import { useState } from "react";
 
-const Lweet = ({ lweetObj, isOwner }) => {
+const Lweet = ({ lweetObj, isOwner, userObj }) => {
    
     const [editing, setEditing] = useState(false);
     const [newLweet, setNewLweet] = useState(lweetObj.text);
-    const [ like, setLike]= useState(0);
+    const [ like, setLike]= useState("");
 
     const desertRef = ref(storage, lweetObj.attachmentUrl);
+
+    const onLike = async () => {
+        const ok = window.confirm("좋아요를 누르시겠습니까?");
+        if (ok){
+            const data = doc(db , `lweets/${lweetObj.id}`);
+            console.log(data.id);
+            const likesDB = await addDoc(collection(db, "likes"));
+           // console.log(likesDB);
+            likesDB.forEach((document) =>
+            {
+                const likeObject = { ...document.data(), id: document.id, post_id:data.id };
+            
+            setLike((prev) => [likeObject, ...prev])
+           
+            console.log(likeObject);
+            
+        });
+    }
+        //     onSnapshot(collection(db,"likes"), (snapshot) =>
+        //     {
+        //     const newLike = snapshot.docs.map((document) => ({
+        //         id:document.id,
+        //         post_id: data.id,
+                
+        //         ...document.data(),
+        //     }));
+        //     console.log(newLike);
+        //     setLike("");
+        // })}
+
+    };
+
+    // const getLweets = async() => {
+    //     const dbLweets = await getDocs(collection(db, "lweets"));
+    //     dbLweets.forEach((document) => {
+    //         const lweetObject = { ...document.data(), id: document.id };
+    //     setLweets((prev) => [lweetObject, ...prev])
+    // });
+    // }
 
     const onDeleteClick = async () => {
         const ok = window.confirm("삭제하시겠습니까?");
@@ -66,8 +105,9 @@ const Lweet = ({ lweetObj, isOwner }) => {
             <button onClick = {onDeleteClick}>Delete Lweet </button>
             <button onClick = {toggleEditing}>Update Lweet </button>
             </>
-            ):(
-                <span onClick={() => { setLike(like + 1); }} > 좋아요 {like}</span>
+            ):( <input type ="submit" value = "like" onClick = {onLike}></input>
+                //<span onClick={() => { setLike(like + 1); }} > 좋아요 {like}</span>
+                //<button onClick = {onLike}> 좋아요2 </button>
             )}
             </>
             )}
